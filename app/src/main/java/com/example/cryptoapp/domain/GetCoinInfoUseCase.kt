10f48a654f4e5a3317e774104cb17077d8fe8f81
ir-sen <irsen.kan@bk.ru>
2021-11-16 13:30:58 +0300

    crete data layout

---
+++ app/src/main/java/com/example/cryptoapp/domain/GetCoinInfoUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.cryptoapp.domain
+
+class GetCoinInfoUseCase(
+
+    private val repository: CoinRepository
+) {
+
+    operator fun invoke(fromSymbol: String) = repository.getCoinInfo(fromSymbol)
+
+}
